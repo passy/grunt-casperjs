6fda7da2ec215efc793c815b70701115e9855215
Pascal Hartig <passy@twitter.com>
2013-09-30 09:03:21 +0100

    Upgrade to casperjs 1.1 beta2

--- tasks/lib/casperjs.js
+++ tasks/lib/casperjs.js
@@ -5,7 +5,7 @@ exports.init = function(grunt) {
 
   exports.casperjs = function(filepath, options, callback) {
 
-    var capserPath = path.join(__dirname, '..', '..', 'lib', 'casperjs', 'casperjs-1.1-beta1', 'bin'),
+    var capserPath = path.join(__dirname, '..', '..', 'lib', 'casperjs', 'casperjs-1.1-beta2', 'bin'),
         command = path.join(capserPath, 'casperjs') + ' test',
         exec = require('child_process').exec,
         phantomBinPath = require('phantomjs').path;
